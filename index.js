5ab1492d77a3f913b3414aac4fdbf19d25b787d1
Scott Keplinger <skeplin17@gmail.com>
2020-03-23 23:50:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function takeANumber (currentLine, newName) {
+  let i = 0
+  for (i = 0; i <= 0; i+= 1) {
+    currentLine.push(newName)
+  }
+  return `Welcome, ${newName}.`+ `You are number ${i} in line.`
+}
