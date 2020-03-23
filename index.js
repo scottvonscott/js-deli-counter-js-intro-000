68b89a1d5404e4952fde8ced7ca91a0549afbf77
Scott Keplinger <skeplin17@gmail.com>
2020-03-23 23:53:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function takeANumber (currentLine, newName) {
-  let i = 0
-  for (i = 0; i <= 0; i+= 1) {
+  for (let i = 0; i <= 0; i+= 1) {
     currentLine.push(newName)
   }
   return `Welcome, ${newName}.` + `You are number ${i} in line.`
