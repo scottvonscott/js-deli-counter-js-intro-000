d9b27e06deefe22c7ec5f4e835ce3be12aef372a
Scott Keplinger <skeplin17@gmail.com>
2020-03-24 01:05:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber (currentLine, newName) {
 }
 
 function nowServing (katzDeliLine) {
-  if (katzDeliLine.length = []) {
+  if (katzDeliLine.length = [0]) {
     return "There is nobody waiting to be served!"
 } else {
     return `Currently serving ${katzDeliLine.shift()}.`
