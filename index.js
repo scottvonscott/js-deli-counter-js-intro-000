4a86ddc1eb806d40f189a1840d813f64a114fbd0
Scott Keplinger <skeplin17@gmail.com>
2020-03-24 01:21:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ function nowServing (katzDeliLine) {
  }
 }
 
-// function nowServing(katzDeliLine) {
-//   return `Currently serving ${katzDeliLine.shift()}.`
-// } (katzDeliLine.length = 0) {
-//     return "There is nobody waiting to be served!"
-//   }
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty."
+} else {
+    return `The line is currently: 1.${line[0]}, 2. ${line[1]}, 3. ${line[2]}`
+ }
+}
